ed69c105d5436477535fd0de5c79ef7c475bedfb
regisf <regisfloret@protonmail.com>
2021-05-26 21:54:21 +0200

    Fail to implement tests

--- tests/utils/test_filecopy.cpp
+++ tests/utils/test_filecopy.cpp
@@ -3,37 +3,55 @@
 
 #include <filesystem>
 #include <gtest/gtest.h>
-#include <fakeit.hpp>
+#include <gmock/gmock.h>
 
-using namespace fakeit;
+#include "../../src/utils/filecopy.hpp"
 
-TEST(test_filecopy, copy_file)
+
+class MockConfig : public Config
 {
-    GTEST_SKIP();
+public:
+    MOCK_METHOD(std::filesystem::path, get_destination_directory, ());
+    MOCK_METHOD(const bool, backup_exists, ());
+    MOCK_METHOD(std::filesystem::path, get_, ());
+    MOCK_METHOD(std::filesystem::path, get_destination_directory, (const std::filesystem::path &path));
+    MOCK_METHOD(const bool, is_destination_dir_exists, (const std::filesystem::path &source));
+};
 
+
+class MockFileCopy : public FileCopy
+{
+public:
+    MOCK_METHOD(void, copy_file, (const std::filesystem::path &source, const std::filesystem::path &dest));
+    MOCK_METHOD(Status, create_parent_directory, (const std::filesystem::path& source));
+    MOCK_METHOD(void ,effective_copy, (const std::filesystem::path &source, const std::filesystem::path &dest));
+};
+
+
+using ::testing::AtLeast;
+using ::testing::Return;
+
+TEST(test_filecopy, copy_file_destination_directory_exists)
+{
     // Arrange
     std::filesystem::path source{"/a/source/file"};
     std::filesystem::path dest{"/another/destination"};
 
-    Mock<IConfig> mock_config;
-    Fake(Dtor(mock_config));
-    std::shared_ptr<IConfig> config(&mock_config.get());
+    std::shared_ptr<MockConfig> mock_config = std::make_shared<MockConfig>();
+    ON_CALL(*mock_config, is_destination_dir_exists(source))
+        .WillByDefault(Return(true));
 
-    When(Method(mock_config, is_destination_dir_exists)).Return(true);
-    When(ConstOverloadedMethod(mock_config, get_destination_directory, std::filesystem::path(const std::filesystem::path &))).Return(dest / source);
+    MockFileCopy file_copy;
+    EXPECT_CALL(file_copy, effective_copy(source, dest));
 
-    FileCopy mock_file_copy{config};
-    Mock<FileCopy> spy(mock_file_copy);
+    file_copy.set_config(mock_config);
 
-    When(OverloadedMethod(spy, create_parent_directories, Status(const std::filesystem::path &)));
-    When(Method(spy, effective_copy));
-
-    FileCopy &file_copy = spy.get();
-
-    // Act
+    // Act + Assert
     file_copy.copy_file(source, dest);
 
-    // Assert
-    VerifyNoOtherInvocations(OverloadedMethod(spy, create_parent_directories, Status(const std::filesystem::path &)));
-    Verify(Method(spy, effective_copy)).Exactly(1_Time);
+}
+
+TEST(test_filecopy, copy_file_destination_directory_doesnt_exist)
+{
+    ASSERT_TRUE(false);
 }
