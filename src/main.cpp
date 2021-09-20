d9eb3a0cb0a43d7d0b05ba676ddf1706cb059674
regisf <regisfloret@protonmail.com>
2021-09-20 20:54:57 +0200

    Add version argument

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,8 @@ enum class StatusCode {
     BackupActionStatus = 2
 };
 
+static const std::string_view CurrentVersion{"1.0.0"};
+
 static void display_usage()
 {
     std::cout << R"(r-backup - Event driven backup system.
@@ -151,6 +153,11 @@ static std::shared_ptr<Config> parse_commandline(int argc, char** argv)
     
     auto cli = lyra::cli() | lyra::help(show_help);
 
+    auto version_group = lyra::command("version", [](const lyra::group &g) {
+        std::cout << "R-Backup : A backup tool. \n    Version " << CurrentVersion << "\n";
+        std::exit(EXIT_SUCCESS);
+    }).help("Display the current version");
+
     auto backup_group = lyra::command("backup", [](const lyra::group& g) {})
             .help("Display backup help")
             .add_argument(lyra::opt(config->verbose)["--verbose"]("Set verbosity to on").optional())
@@ -161,6 +168,7 @@ static std::shared_ptr<Config> parse_commandline(int argc, char** argv)
             .add_argument(lyra::opt(config_file, "configuration_file")["--config-file"]("set the config file that contains ").optional());
 
     cli.add_argument(backup_group);
+    cli.add_argument(version_group);
 
     if (auto result = cli.parse({ argc, argv }); !result) {
         std::cerr << "Error in command line: " << result.errorMessage()
@@ -173,23 +181,12 @@ static std::shared_ptr<Config> parse_commandline(int argc, char** argv)
         std::exit(EXIT_SUCCESS);
     }
 
-//    if (strategy.size()) {
-//        if (!nth) {
-//            std::cerr << "Error missing "
-//        }
-//        config->strategy = StrategyValue(strategy, nth);
-//    }
-
-
     return config;
 }
 
 int main(int argc, char** argv)
 {
     auto config = parse_commandline(argc, argv);
-
-    std::cout << "Verbose = " << config->verbose << "\n"
-               << "Dry run = " << config->dry_run << "\n";
     StatusCode ret_val = StatusCode::DefaultStatusCode;
 
     try
