f9f9e2d10da9ed284e38d0e949beafe61c7a2dbb
regisf <regisfloret@protonmail.com>
2022-06-18 14:39:44 +0200

    Improve mixin tests

---
+++ mixins/source/one/exclude_me/onetwothree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by regis on 18/06/2022.
+//
+
+#include "onetwothree.hpp"
---
+++ mixins/source/one/exclude_me/onetwothree.hpp
@@ -0,0 +1,15 @@
+//
+// Created by regis on 18/06/2022.
+//
+
+#ifndef R_BACKUP_ONETWOTHREE_HPP
+#define R_BACKUP_ONETWOTHREE_HPP
+
+
+class onetwothree
+{
+
+};
+
+
+#endif //R_BACKUP_ONETWOTHREE_HPP
